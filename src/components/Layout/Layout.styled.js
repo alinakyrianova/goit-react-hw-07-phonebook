97c1df40b04291d57e50f8235123585a7f3c8f8e
alinakyrianova <alina.kyrianova@gmail.com>
2023-10-03 16:56:32 +0200

    test

---
+++ src/components/Layout/Layout.styled.js
@@ -0,0 +1,28 @@
+import { Suspense } from 'react';
+import { Container } from './Layout.styled';
+import { Header } from 'components/Header/Header';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 8px;
+  padding-bottom: 32px;
+
+  @media screen and (min-width: 767px) {
+    width: 746px;
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-top: 15px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 1200px;
+    padding-left: 30px;
+    padding-right: 30px;
+    padding-top: 15px;
+  }
+`;
